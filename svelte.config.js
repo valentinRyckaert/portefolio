0429440004de4411c9027e3bda795abd6ac44f07
user <user@fedora.home>
2024-10-16 17:42:06 +0200

    change static files hierarchie

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: () => {
+				return;
+			}
+		},
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/portefolio' : '',
+		}
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
 	}
 };
 
