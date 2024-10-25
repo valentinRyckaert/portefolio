d8ed684cb304b2e85dc5be57ed78e6c35f971961
user <user@fedora.home>
2024-10-25 20:27:24 +0200

    add certification

--- src/components/competences/Certifications.svelte
+++ src/components/competences/Certifications.svelte
@@ -4,6 +4,12 @@
 
 <div class="row row-cols-1 row-cols-lg-2 justify-content-center g-3">
     <div class="col">
+        <UneCertification imageLink="images/c_api_doc.pdf" title="Open Source Technical Documentation Essentials (Linux Fondation)" listTools={['Documentation d\'API',"Documentation Open-Source"]}/>
+    </div>
+    <div class="col">
+        <UneCertification imageLink="images/c_backend.png" title="Backend development and APIs (FreeCodeCamp)" listTools={["Node.js","Express.js","MongoDB"]}/>
+    </div>
+    <div class="col">
         <UneCertification imageLink="images/c_anssi.png" title="MOOC cybersécurité (ANSSI)" listTools={["sécurité des mots de passe","sécurité physique","preuve informatique","bonnes pratiques cyber"]}/>
     </div>
     <div class="col">
