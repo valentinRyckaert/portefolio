5ed4ea30fbd88f23b153da9f1a61b4437eb89dac
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-07-14 16:09:59 +0200

    Feat: add certification and update formation

--- src/routes/formation/+page.svelte
+++ src/routes/formation/+page.svelte
@@ -6,17 +6,24 @@
     <h2 class="text-center">Ma formation</h2>
 </div>
 <div class="row m-4">
-    <TextImage imageLink="images/logo-chartreux.png" imagePos="right" alt="logo chartreux">
-        <h4>Baccalauréat général à l'Institution des Chartreux</h4>
+    <TextImage imageLink="images/cpe-lyon.jpg" imagePos="right" alt="logo CPE Lyon">
+        <h4>Diplôme d'ingénieur en Cybersécurité à CPE Lyon (2025-2028)</h4>
         <br>
-        <h5>Mathématiques-NSI</h5>
-        <h5>mention Très Bien</h5>
+        <h5>Formation en 3 ans dont 2 en alternance</h5>
     </TextImage>
 </div>
 <div class="row m-4">
     <TextImage imageLink="images/logo-supalta.jpg" imagePos="left" alt="logo SupAlta">
-        <h4>BTS SIO à l'Institution des Chartreux</h4>
+        <h4>BTS SIO à l'Institution des Chartreux (2025)</h4>
         <br>
         <h5>spécialité SLAM (Solutions Logicielles et Applications Métiers)</h5>
+    </TextImage>
+</div>
+<div class="row m-4">
+    <TextImage imageLink="images/logo-chartreux.png" imagePos="right" alt="logo chartreux">
+        <h4>Baccalauréat général à l'Institution des Chartreux (2023)</h4>
+        <br>
+        <h5>Mathématiques-NSI</h5>
+        <h5>mention Très Bien</h5>
     </TextImage>
 </div>
